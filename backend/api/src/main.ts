b4cd883a6a7ade28c99bfa5cc1a1531f2ef5de26
Alison Simiao <alison.simiao@gmail.com>
2025-09-16 22:09:57 -0300

    http simples

--- backend/api/src/main.ts
+++ backend/api/src/main.ts
@@ -31,7 +31,7 @@ async function bootstrap(): Promise<void> {
     optionsSuccessStatus: 204,
   });
 
-  await app.listen(process.env.PORT ?? 3000, () => {
+  await app.listen(process.env.PORT ?? 3000, '0.0.0.0', () => {
     console.log(`Server is running on port ${process.env.PORT ?? 3000}`);
   });
 }
